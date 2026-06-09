77befdb53d5b0d7b1247758d0431d596c4635059
Alex Schwartz <alexschwartz01@gmail.com>
2026-06-09 10:45:11 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,1 +1,6 @@
-echo Hello World
+if ([Environment]::OSVersion.Platform -ne "Win32NT" -or $env:PROCESSOR_ARCHITECTURE -eq "x86") {
+    Write-Host "This npm module is only for Microsoft Windows x64"
+    exit 1
+}
+
+echo 'hi'

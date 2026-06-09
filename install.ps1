8e66916fe35c1f6e1a644f4c5dbaa094ff28911b
Alex Schwartz <alexschwartz01@gmail.com>
2026-06-09 13:56:17 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -23,8 +23,8 @@ if ($oldPath -split [IO.Path]::PathSeparator -notcontains $addPath) {
 $bashProfile = "$env:USERPROFILE\.bash_profile"
 if (-not (Test-Path $bashProfile)) {
     New-Item -ItemType File -Path $bashProfile -Force | Out-Null
-}
-if (-not (Select-String -Path $bashProfile -Pattern "alias nodeswitch=" -Quiet)) {
+    Add-Content -Path $bashProfile -Value 'alias nodeswitch="source nodeswitch"'
+} elseif (-not (Select-String -Path $bashProfile -Pattern "alias nodeswitch=" -Quiet)) {
     Add-Content -Path $bashProfile -Value 'alias nodeswitch="source nodeswitch"'
 }
 

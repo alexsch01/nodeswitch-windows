51a5a726d302953bb2ce8954ccf0766536298755
Alex Schwartz <alexschwartz01@gmail.com>
2026-09-07 11:49:27 -0400

    formatting

--- bin/nodeswitch.cmd
+++ bin/nodeswitch.cmd
@@ -19,10 +19,10 @@ if not "%1" == "" (
                 ) else (
                     if exist %AppData%\nodeswitch\%2 (
                         if not defined nodeswitchDefaultPATH (
-			    set "nodeswitchDefaultPATH=%PATH%"
-			    set "PATH=%AppData%\nodeswitch\%2;%PATH%"
+							set "nodeswitchDefaultPATH=%PATH%"
+							set "PATH=%AppData%\nodeswitch\%2;%PATH%"
                         ) else (
-			    set "PATH=%AppData%\nodeswitch\%2;%nodeswitchDefaultPATH%"
+							set "PATH=%AppData%\nodeswitch\%2;%nodeswitchDefaultPATH%"
                         )
                     ) else (
                         echo Node version not installed

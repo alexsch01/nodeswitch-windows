3bfa8a903070fc32a074beafc865fd4de7bb17f0
Alex Schwartz <alexschwartz01@gmail.com>
2026-09-08 08:02:53 -0400

    rename to nodeswitchUseParameter

--- bin/nodeswitch.ps1
+++ bin/nodeswitch.ps1
@@ -1,21 +1,20 @@
-$nodeswitch1stParameter = $args[0]
-$nodeswitch2ndParameter = $args[1]
+$nodeswitchUseParameter = $args[1]
 
 if ( $args[2] -ne $null ) {
     echo "Incorrect command"
 } else {
-    if ( $nodeswitch1stParameter -eq "use" -and $nodeswitch2ndParameter -ne $null ) {
-        if ( $nodeswitch2ndParameter -eq "default" ) {
+    if ( $args[0] -eq "use" -and $nodeswitchUseParameter -ne $null ) {
+        if ( $nodeswitchUseParameter -eq "default" ) {
             if ( $global:nodeswitchDefaultPATH -ne $null ) {
                 $env:Path = $global:nodeswitchDefaultPATH
             }
         } else {
-            if ( Test-Path -Path $env:AppData\nodeswitch\$nodeswitch2ndParameter ) {
+            if ( Test-Path -Path $env:AppData\nodeswitch\$nodeswitchUseParameter ) {
                 if ( $global:nodeswitchDefaultPATH -eq $null ) {
                     $global:nodeswitchDefaultPATH = $env:Path
-                    $env:Path = "$env:AppData\nodeswitch\$nodeswitch2ndParameter;$env:AppData\nodeswitch\$nodeswitch2ndParameter\globalPrefix;$env:Path"
+                    $env:Path = "$env:AppData\nodeswitch\$nodeswitchUseParameter;$env:AppData\nodeswitch\$nodeswitchUseParameter\globalPrefix;$env:Path"
                 } else {
-                    $env:Path = "$env:AppData\nodeswitch\$nodeswitch2ndParameter;$env:AppData\nodeswitch\$nodeswitch2ndParameter\globalPrefix;$global:nodeswitchDefaultPATH"
+                    $env:Path = "$env:AppData\nodeswitch\$nodeswitchUseParameter;$env:AppData\nodeswitch\$nodeswitchUseParameter\globalPrefix;$global:nodeswitchDefaultPATH"
                 }
             } else {
                 echo "Node version not installed"

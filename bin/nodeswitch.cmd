4c3b90ff9f9b8b5c02f683a5e3466851139e822e
Alex Schwartz <alexschwartz01@gmail.com>
2026-09-08 07:54:39 -0400

    hardcode curl and tar to Windows paths

--- bin/nodeswitch.cmd
+++ bin/nodeswitch.cmd
@@ -30,14 +30,14 @@ if not "%1" == "" (
                 )
             ) else if "%1" == "add" (
                 if not exist %AppData%\nodeswitch\%2 (
-                    curl -f https://nodejs.org/download/release/v%2/ > nul 2>&1 && (
-                        curl -s -o %AppData%\nodeswitch\%2.zip https://nodejs.org/download/release/v%2/node-v%2-win-x64.zip > nul || (
+                    C:\Windows\System32\curl -f https://nodejs.org/download/release/v%2/ > nul 2>&1 && (
+                        C:\Windows\System32\curl -s -o %AppData%\nodeswitch\%2.zip https://nodejs.org/download/release/v%2/node-v%2-win-x64.zip > nul || (
                             del %AppData%\nodeswitch\%2.zip
                             echo Node version not created
                             exit
                         )
 
-                        tar -xf %AppData%\nodeswitch\%2.zip -C %AppData%\nodeswitch || (
+                        C:\Windows\System32\tar -xf %AppData%\nodeswitch\%2.zip -C %AppData%\nodeswitch || (
                             del %AppData%\nodeswitch\%2.zip
                             rd /s /q %AppData%\nodeswitch\node-v%2-win-x64
                             echo Node version not created

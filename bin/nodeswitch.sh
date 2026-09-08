1b1f4af8394dd819db427edc2069bbf397b50d6f
Alex Schwartz <alexschwartz01@gmail.com>
2026-09-08 07:54:00 -0400

    call cmd from sh

--- bin/nodeswitch.sh
+++ bin/nodeswitch.sh
@@ -1,78 +1,26 @@
 nodeswitchAPPDATA=$(cygpath -u $APPDATA)
 
-if [ "$1" != "" ]; then
-    if [ "$2" != "" ]; then
-        if [ "$3" != "" ]; then
-            echo "Incorrect command"
+if [ "$3" != "" ]; then
+    echo "Incorrect command"
+else
+    if [ "$1" = "use" ] && [ "$2" != "" ]; then
+        if [ "$2" = "default" ]; then
+            if [ ! -z "$nodeswitchDefaultPATH" ]; then
+                export PATH=$nodeswitchDefaultPATH
+            fi
         else
-            if [ "$1" = "remove" ]; then
-                if [ -d "$nodeswitchAPPDATA/nodeswitch/$2" ]; then
-                    rm -rf $nodeswitchAPPDATA/nodeswitch/$2
+            if [ -d "$nodeswitchAPPDATA/nodeswitch/$2" ]; then
+                if [ -z "$nodeswitchDefaultPATH" ]; then
+                    export nodeswitchDefaultPATH=$PATH
+                    export PATH=$nodeswitchAPPDATA/nodeswitch/$2:$nodeswitchAPPDATA/nodeswitch/$2/globalPrefix:$PATH
                 else
-                    echo "Node version not installed"
-                fi
-            elif [ "$1" = "use" ]; then
-                if [ "$2" = "default" ]; then
-                    if [ ! -z "$nodeswitchDefaultPATH" ]; then
-                        export PATH=$nodeswitchDefaultPATH
-                    fi
-                else
-                    if [ -d "$nodeswitchAPPDATA/nodeswitch/$2" ]; then
-                        if [ -z "$nodeswitchDefaultPATH" ]; then
-                            export nodeswitchDefaultPATH=$PATH
-                            export PATH=$nodeswitchAPPDATA/nodeswitch/$2:$nodeswitchAPPDATA/nodeswitch/$2/globalPrefix:$PATH
-                        else
-                            export PATH=$nodeswitchAPPDATA/nodeswitch/$2:$nodeswitchAPPDATA/nodeswitch/$2/globalPrefix:$nodeswitchDefaultPATH
-                        fi
-                    else
-                        echo "Node version not installed"
-                    fi
-                fi
-            elif [ "$1" = "add" ]; then
-                if [ ! -d "$nodeswitchAPPDATA/nodeswitch/$2" ]; then
-                    /c/Windows/System32/curl -f https://nodejs.org/download/release/v$2/ &>/dev/null
-                    if [ $? -eq 0 ]; then
-                        /c/Windows/System32/curl -s -o $nodeswitchAPPDATA/nodeswitch/$2.zip https://nodejs.org/download/release/v$2/node-v$2-win-x64.zip > /dev/null
-
-                        if [ $? -ne 0 ]; then
-                            rm $nodeswitchAPPDATA/nodeswitch/$2.zip
-                            echo "Node version not created"
-                            return
-                        fi
-
-                        unzip -d $nodeswitchAPPDATA/nodeswitch -o $nodeswitchAPPDATA/nodeswitch/$2.zip > /dev/null
-
-                        if [ $? -ne 0 ]; then
-                            rm $nodeswitchAPPDATA/nodeswitch/$2.zip
-                            rm -rf $nodeswitchAPPDATA/nodeswitch/node-v$2-win-x64
-                            echo "Node version not created"
-                            return
-                        fi
-
-                        rm $nodeswitchAPPDATA/nodeswitch/$2.zip
-                        mv $nodeswitchAPPDATA/nodeswitch/node-v$2-win-x64 $nodeswitchAPPDATA/nodeswitch/$2
-
-                        mkdir $nodeswitchAPPDATA/nodeswitch/$2/globalPrefix
-                        echo "prefix=$APPDATA\\nodeswitch\\$2\\globalPrefix" > $nodeswitchAPPDATA/nodeswitch/$2/node_modules/npm/npmrc
-                    else
-                        echo "Node version not found"
-                    fi
-                else
-                    echo "Node version already added"
+                    export PATH=$nodeswitchAPPDATA/nodeswitch/$2:$nodeswitchAPPDATA/nodeswitch/$2/globalPrefix:$nodeswitchDefaultPATH
                 fi
             else
-                echo "Incorrect command"
+                echo "Node version not installed"
             fi
         fi
     else
-        if [ "$1" = "list" ]; then
-            dir -1 $nodeswitchAPPDATA/nodeswitch
-        elif [ "$1" = "path" ]; then
-            echo "$APPDATA\nodeswitch"
-        else
-            echo "Incorrect command"
-        fi
+        "$(dirname ${BASH_SOURCE[0]})/nodeswitch.cmd" "$@"
     fi
-else
-    echo "Incorrect command"
 fi
